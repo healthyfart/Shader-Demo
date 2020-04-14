5f2c1d533670a92592f8e8dc85c31bf10b68aaf6
healthyfart <aa1996709tw@gmail.com>
2020-04-14 11:07:50 +0800

    add more shader example

---
+++ UP test/Assets/Scripts/vertex.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vertex : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float displacementAmout;
+    private MeshRenderer meshRenderer;
+    
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        displacementAmout = Mathf.Lerp(displacementAmout, 0, Time.deltaTime);
+        meshRenderer.material.SetFloat("_Amout", displacementAmout);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            displacementAmout += 1f;
+        }
+    }
+}
